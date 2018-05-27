6f66ec7174140bfb8763eecd6637ea72214ca42d
splittydev <splittydev@protonmail.com>
2018-05-27 17:04:51 +0200

    Transpile subtle.js

--- subtle.js
+++ subtle.js
@@ -1,176 +1,193 @@
-(() => {
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+(function () {
 
     // Subtle particle
-    class Particle {
-        constructor(state) {
+    var Particle = function () {
+        function Particle(state) {
+            _classCallCheck(this, Particle);
+
             this.reinitialize(state);
         }
 
         // Initialize variables
-        reinitialize(state) {
 
-            // Opacity starts with 0
-            this.a = 0;
 
-            // Size starts with 0
-            this.r = 0;
+        _createClass(Particle, [{
+            key: 'reinitialize',
+            value: function reinitialize(state) {
+                var _this = this;
 
-            // Random hue
-            this.c = Math.random() * 360;
+                // Opacity starts with 0
+                this.a = 0;
 
-            // Random speed within [speed/2, speed]
-            this.s = (state.conf.speed + (Math.random() * state.conf.speed)) / 2.0;
+                // Size starts with 0
+                this.r = 0;
 
-            // Random position
-            const calculatePosition = () => {
-                const getRandomIntInclusive = (min, max) => {
-                    min = Math.ceil(min);
-                    max = Math.floor(max);
-                    return Math.floor(Math.random() * (max - min + 1)) + min;
+                // Random hue
+                this.c = Math.random() * 360;
+
+                // Random speed within [speed/2, speed]
+                this.s = (state.conf.speed + Math.random() * state.conf.speed) / 2.0;
+
+                // Random position
+                var calculatePosition = function calculatePosition() {
+                    var getRandomIntInclusive = function getRandomIntInclusive(min, max) {
+                        min = Math.ceil(min);
+                        max = Math.floor(max);
+                        return Math.floor(Math.random() * (max - min + 1)) + min;
+                    };
+                    var pad = state.conf.contain ? state.conf.size : 0;
+                    _this.x = getRandomIntInclusive(pad, state.width - pad * 2);
+                    _this.y = getRandomIntInclusive(pad, state.height - pad * 2);
                 };
-                const pad = state.conf.contain ? state.conf.size : 0;
-                this.x = getRandomIntInclusive(pad, state.width - pad * 2);
-                this.y = getRandomIntInclusive(pad, state.height - pad * 2);
-            };
-            calculatePosition();
+                calculatePosition();
 
-            // Random rotation in radians
-            this.rad = Math.random() * Math.PI * 2;
+                // Random rotation in radians
+                this.rad = Math.random() * Math.PI * 2;
 
-            // Recalculate position while collisions with
-            // exclusion target are detected.
-            if (state.ex !== null) {
-                const cBounds = state.canvas.getBoundingClientRect();
-                const exBounds = state.ex.getBoundingClientRect();
-                while (subtle_intersects(
-                    cBounds.left + this.x - (state.conf.size),
-                    cBounds.left + this.x + (state.conf.size * 2),
-                    cBounds.top + this.y - (state.conf.size),
-                    cBounds.top + this.y + (state.conf.size * 2),
-                    exBounds.left,
-                    exBounds.right,
-                    exBounds.top,
-                    exBounds.bottom
-                )) calculatePosition();
-            }
-        }
-
-        // Update particle
-        update(state) {
-
-            // Increase size by speed
-            this.r += this.s;
-
-            // Increase hue by 1
-            this.c = (this.c + 1) % 360;
-
-            // r <= 25%
-            if (this.r < state.conf.size * 0.25) {
-                this.a = (this.r / (state.conf.size * 0.25));
+                // Recalculate position while collisions with
+                // exclusion target are detected.
+                if (state.ex !== null) {
+                    var cBounds = state.canvas.getBoundingClientRect();
+                    var exBounds = state.ex.getBoundingClientRect();
+                    while (subtle_intersects(cBounds.left + this.x - state.conf.size, cBounds.left + this.x + state.conf.size * 2, cBounds.top + this.y - state.conf.size, cBounds.top + this.y + state.conf.size * 2, exBounds.left, exBounds.right, exBounds.top, exBounds.bottom)) {
+                        calculatePosition();
+                    }
+                }
             }
 
-            // r >= 25% && r <= 75%
-            else if (this.r >= state.conf.size * 0.25 && this.r <= state.conf.size * 0.75) {
-                this.a = 1;
-            }
+            // Update particle
 
-            // r >= 75% && r <= 100%
-            else if (this.r >= state.conf.size * 0.75 && this.r <= state.conf.size) {
-                this.a = (state.conf.size - this.r) / (state.conf.size * 0.25);
-            }
+        }, {
+            key: 'update',
+            value: function update(state) {
 
-            // r > 100%
-            else {
-                this.reinitialize(state);
-            }
-        }
+                // Increase size by speed
+                this.r += this.s;
 
-        // Render particle
-        render(state) {
+                // Increase hue by 1
+                this.c = (this.c + 1) % 360;
 
-            // Push state
-            state.context.save();
-
-            // Calculate lightness and saturation
-            const lit = state.conf.lightness * 100.0;
-            const sat = state.conf.saturation * 100.0;
-
-            // Set stroke style
-            state.context.strokeStyle = `hsla(${this.c},${sat}%,${lit}%,${this.a})`;
-
-            // Begin path
-            state.context.beginPath();
-
-            // Switch on modes
-            switch (state.conf.mode) {
-
-                // Circle
-                case Modes.circle: {
-
-                    // Draw circle
-                    state.context.arc(
-                        this.x, // x
-                        this.y, // y
-                        this.r, // r
-                        0, // sAngle
-                        2 * Math.PI, // eAngle
-                    );
-
-                    break;
+                // r <= 25%
+                if (this.r < state.conf.size * 0.25) {
+                    this.a = this.r / (state.conf.size * 0.25);
                 }
 
-                // Square
-                case Modes.square: {
-
-                    // Test if rotation is randomized
-                    if (state.conf.randomizeRotation) {
-
-                        // Calculate center
-                        const cx = this.x + this.r / 2;
-                        const cy = this.y + this.r / 2;
-
-                        // Translate to center
-                        state.context.translate(cx, cy);
-
-                        // Rotate canvas
-                        state.context.rotate(this.rad);
+                // r >= 25% && r <= 75%
+                else if (this.r >= state.conf.size * 0.25 && this.r <= state.conf.size * 0.75) {
+                        this.a = 1;
                     }
 
-                    // Draw rectangle
-                    state.context.rect(
-                        state.conf.randomizeRotation ? 0 : this.x, // x
-                        state.conf.randomizeRotation ? 0 : this.y, // y
-                        this.r, // w
-                        this.r // h
-                    );
-                
-                    break;
-                }
+                    // r >= 75% && r <= 100%
+                    else if (this.r >= state.conf.size * 0.75 && this.r <= state.conf.size) {
+                            this.a = (state.conf.size - this.r) / (state.conf.size * 0.25);
+                        }
+
+                        // r > 100%
+                        else {
+                                this.reinitialize(state);
+                            }
             }
 
-            // Draw stroke
-            state.context.stroke();
+            // Render particle
 
-            // Pop state
-            state.context.restore();
-        }
-    }
+        }, {
+            key: 'render',
+            value: function render(state) {
+
+                // Push state
+                state.context.save();
+
+                // Calculate lightness and saturation
+                var lit = state.conf.lightness * 100.0;
+                var sat = state.conf.saturation * 100.0;
+
+                // Set stroke style
+                state.context.strokeStyle = 'hsla(' + this.c + ',' + sat + '%,' + lit + '%,' + this.a + ')';
+
+                // Begin path
+                state.context.beginPath();
+
+                // Switch on modes
+                switch (state.conf.mode) {
+
+                    // Circle
+                    case Modes.circle:
+                        {
+
+                            // Draw circle
+                            state.context.arc(this.x, // x
+                            this.y, // y
+                            this.r, // r
+                            0, // sAngle
+                            2 * Math.PI // eAngle
+                            );
+
+                            break;
+                        }
+
+                    // Square
+                    case Modes.square:
+                        {
+
+                            // Test if rotation is randomized
+                            if (state.conf.randomizeRotation) {
+
+                                // Calculate center
+                                var cx = this.x + this.r / 2;
+                                var cy = this.y + this.r / 2;
+
+                                // Translate to center
+                                state.context.translate(cx, cy);
+
+                                // Rotate canvas
+                                state.context.rotate(this.rad);
+                            }
+
+                            // Draw rectangle
+                            state.context.rect(state.conf.randomizeRotation ? 0 : this.x, // x
+                            state.conf.randomizeRotation ? 0 : this.y, // y
+                            this.r, // w
+                            this.r // h
+                            );
+
+                            break;
+                        }
+                }
+
+                // Draw stroke
+                state.context.stroke();
+
+                // Pop state
+                state.context.restore();
+            }
+        }]);
+
+        return Particle;
+    }();
 
     // Setup
+
+
     function subtle_setup(state) {
 
         // Fitting function
-        const subtle_fit = () => {
-            const bounds = state.el.getBoundingClientRect();
-            state.canvas.style.left = `${bounds.left}px`;
-            state.canvas.style.top = `${bounds.top}px`;
-            state.canvas.style.width = `${bounds.width}px`;
-            state.canvas.style.height = `${bounds.height}px`;
+        var subtle_fit = function subtle_fit() {
+            var bounds = state.el.getBoundingClientRect();
+            state.canvas.style.left = bounds.left + 'px';
+            state.canvas.style.top = bounds.top + 'px';
+            state.canvas.style.width = bounds.width + 'px';
+            state.canvas.style.height = bounds.height + 'px';
             state.canvas.width = bounds.width;
             state.canvas.height = bounds.height;
             state.width = bounds.width;
             state.height = bounds.height;
-        }
+        };
 
         // Fit canvas and refit on resize
         subtle_fit() || window.addEventListener('resize', subtle_fit);
@@ -182,7 +199,9 @@
         state.context = state.canvas.getContext('2d');
 
         // Spawn particles
-        state.particles = Array.from({length: state.conf.count}).map(_ => new Particle(state));
+        state.particles = Array.from({ length: state.conf.count }).map(function (_) {
+            return new Particle(state);
+        });
     }
 
     // Rectanglular intersection detection
@@ -192,7 +211,9 @@
 
     // Update scene
     function subtle_update(state) {
-        state.particles.forEach(p => p.update(state));
+        state.particles.forEach(function (p) {
+            return p.update(state);
+        });
     }
 
     // Render scene
@@ -202,7 +223,9 @@
         state.context.clearRect(0, 0, state.width, state.height);
 
         // Render all particles
-        state.particles.forEach(p => p.render(state));
+        state.particles.forEach(function (p) {
+            return p.render(state);
+        });
     }
 
     // Present scene
@@ -218,19 +241,19 @@
         subtle_render(state);
 
         // Request next loop iteration
-        window.requestAnimationFrame(function() {
+        window.requestAnimationFrame(function () {
             subtle_present(state);
         });
     }
 
     // Render modes
-    const Modes = {
+    var Modes = {
         circle: 1,
-        square: 2,
+        square: 2
     };
 
     // Default configuration
-    const DefaultConf = {
+    var DefaultConf = {
         target: 'body',
         exclude: null,
         size: 32,
@@ -240,12 +263,12 @@
         saturation: 0.25,
         randomizeRotation: false,
         contain: false,
-        mode: Modes.square,
+        mode: Modes.square
     };
-    
+
     // Subtle object
-    const Subtle = {
-        mode: Modes,
+    var Subtle = {
+        mode: Modes
     };
 
     /**
@@ -254,25 +277,25 @@
     Subtle.mount = function mount(conf) {
 
         // Create new canvas
-        const canvas = document.createElement('canvas');
+        var canvas = document.createElement('canvas');
 
         // Style appropriately
         canvas.style.position = 'absolute';
         canvas.style.zIndex = '-1';
 
         // Merge configuration
-        const finalConf = Object.assign({}, DefaultConf, conf);
+        var finalConf = Object.assign({}, DefaultConf, conf);
 
         // Get target and exclusion elements
-        const targetEl = document.querySelector(finalConf.target || 'body');
-        const excludeEl = finalConf.exclude ? document.querySelector(finalConf.exclude) : null;
+        var targetEl = document.querySelector(finalConf.target || 'body');
+        var excludeEl = finalConf.exclude ? document.querySelector(finalConf.exclude) : null;
 
         // Initialize state
-        const state = {
+        var state = {
             canvas: canvas,
             conf: finalConf,
             el: targetEl,
-            ex: excludeEl,
+            ex: excludeEl
         };
 
         // Add canvas to target element
